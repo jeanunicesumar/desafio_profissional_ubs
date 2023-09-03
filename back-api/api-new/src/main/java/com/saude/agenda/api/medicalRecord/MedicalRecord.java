665f1e74263280d506a3a833d54ef996b8c656a0
JS-Develope <jeanaparecido21@gmail.com>
2023-09-03 15:55:31 -0300

    Atualizando

--- back-api/api-new/src/main/java/com/saude/agenda/api/medicalRecord/MedicalRecord.java
+++ back-api/api-new/src/main/java/com/saude/agenda/api/medicalRecord/MedicalRecord.java
@@ -1,12 +1,8 @@
 package com.saude.agenda.api.medicalRecord;
 
-import com.saude.agenda.api.address.Address;
-import com.saude.agenda.api.person.Gender;
-import com.saude.agenda.api.person.Person;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -22,7 +18,9 @@ public class MedicalRecord {
 
     @Column(nullable = false)
     private Long id;
+
     private String description;
+
     private Date medicalRecordDate;
 
     public MedicalRecord (
