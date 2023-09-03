62de0728b6227e44e5eaa0323e69909926543606
rhayssaandretto <rha.andretto@gmail.com>
2023-09-03 13:56:03 -0300

    back API - Admin

---
+++ back-api/api-new/src/main/java/com/saude/agenda/api/admin/dto/AdminDto.java
@@ -0,0 +1,25 @@
+package com.saude.agenda.api.admin.dto;
+
+import com.saude.agenda.api.address.dto.AddressDto;
+import com.saude.agenda.api.person.Gender;
+import com.saude.agenda.api.person.dto.PersonDto;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+
+public class AdminDto extends PersonDto {
+   public AdminDto (@NotBlank Long id, @NotBlank String name, @NotBlank String motherName,
+                    @NotBlank String fatherName, @NotNull Date birthDate, @NotBlank String birthCity,
+                    @NotBlank String birthUf, @NotBlank String email, @NotNull @Valid Gender gender,
+                    @NotBlank String ddd, @NotBlank String phone, @NotNull String cpf,
+                    @NotNull Boolean active, @Valid AddressDto address){
+       super(id, name, motherName, fatherName, birthDate, birthCity, birthUf, email, gender, ddd, phone, cpf, active, address);
+   }
+}
