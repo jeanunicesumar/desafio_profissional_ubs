8675c62ca86f3a91886796157e99abfdb78ea99f
ViniciusMVilela <viniciusvilela10@gmail.com>
2023-09-13 08:55:52 -0300

    [feat] PatientSerive and PatientController updates

--- back-api/api-new/src/main/java/com/saude/agenda/api/patient/PatientController.java
+++ back-api/api-new/src/main/java/com/saude/agenda/api/patient/PatientController.java
@@ -1,9 +1,15 @@
 package com.saude.agenda.api.patient;
 
 
+import com.saude.agenda.api.patient.dto.PatientDto;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("patient")
@@ -11,5 +17,20 @@ public class PatientController {
     @Autowired
     private PatientService service;
 
+    @GetMapping
+    public List<PatientDto> getAll() {
+        return service.getAll();
+    }
 
+    @GetMapping("/{id}")
+    public PatientDto getById(@PathVariable Long id) {
+        return service.getById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<PatientDto> register(@RequestBody @Valid PatientDto data, UriComponentsBuilder uriBuilder) {
+        PatientDto patient = service.register(data);
+        URI location = uriBuilder.path("/patient/{id}").buildAndExpand(patient.getId()).toUri();
+        return ResponseEntity.created(location).body(patient);
+    }
 }
--- back-api/api-new/src/main/java/com/saude/agenda/api/patient/PatientService.java
+++ back-api/api-new/src/main/java/com/saude/agenda/api/patient/PatientService.java
@@ -2,16 +2,40 @@ package com.saude.agenda.api.patient;
 
 import com.saude.agenda.api.exception.PatientNotFoundException;
 import com.saude.agenda.api.patient.dto.PatientDto;
+import jakarta.persistence.EntityExistsException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-
+@Service
 public class PatientService {
 
     @Autowired
     private PatientRepository patientRepository;
 
+    @Autowired
+    private PatientAdapter adapter;
+
+    public List<PatientDto> getAll() {
+        return patientRepository.findAll().stream().map(this::getPatientDto).toList();
+    }
+
+    public PatientDto getPatientDto(Patient patient) {
+        return  adapter.fromEntity(patient);
+    }
+
+    public PatientDto register(PatientDto data) {
+        Patient patient = adapter.fromDto(data);
+        patientRepository.save(patient);
+        return adapter.fromEntity(patient);
+    }
+
+    public PatientDto getById(Long id) {
+        Patient patient = patientRepository.findById(id)
+                .orElseThrow(() -> new EntityExistsException("Patient not found"));
+        return adapter.fromEntity(patient);
+    }
 
 //find Patient by Id
     public Patient findPatientById(Long patientId) throws PatientNotFoundException {
