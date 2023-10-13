aa5cb3684a70a6f0b02710e48ed89cbb4ff24b84
Cassia <ra-22265008-2@alunos.unicesumar.edu.br>
2023-10-12 21:38:42 -0300

    login quase pronto

--- painel-gestao/painel-gestao/src/app/app.module.ts
+++ painel-gestao/painel-gestao/src/app/app.module.ts
@@ -4,19 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
-<<<<<<< HEAD
-=======
 import { LoginComponent } from './componentes/login/login.component';
->>>>>>> 3388b5e069a62b6159069adefff6dc3206402ed6
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-<<<<<<< HEAD
-=======
     LoginComponent,
->>>>>>> 3388b5e069a62b6159069adefff6dc3206402ed6
   ],
   imports: [
     BrowserModule,
