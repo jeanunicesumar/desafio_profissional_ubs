86e0578d52d01673de19b7007ebbf1b509850844
jeanunicesumar <jeansoares@alunos.unicesumar.edu.br>
2023-10-18 22:16:53 -0300

    Criando login pelo cpf

--- back-api/api-new/src/main/java/com/saude/agenda/api/person/PersonService.java
+++ back-api/api-new/src/main/java/com/saude/agenda/api/person/PersonService.java
@@ -1,13 +1,18 @@
 package com.saude.agenda.api.person;
 
-import com.saude.agenda.api.doctor.Doctor;
-import com.saude.agenda.api.doctor.dto.DoctorDto;
-import com.saude.agenda.api.doctor.dto.DoctorLoginDto;
+import com.saude.agenda.api.admin.Admin;
+import com.saude.agenda.api.admin.AdminAdapter;
+import com.saude.agenda.api.admin.AdminService;
+import com.saude.agenda.api.admin.dto.AdminDto;
+import com.saude.agenda.api.doctor.DoctorAdapter;
+import com.saude.agenda.api.doctor.DoctorService;
 import com.saude.agenda.api.helper.HashPassword;
 import com.saude.agenda.api.person.dto.PersonLoginDto;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class PersonService {
@@ -15,17 +20,34 @@ public class PersonService {
     @Autowired
     private PersonRepository repository;
 
-    // TODO: Adapter
+    @Autowired
+    private AdminService adminService;
+
+    @Autowired
+    private DoctorService doctorService;
+
+    @Autowired
+    private AdminAdapter adminAdapter;
+
+    @Autowired
+    private DoctorAdapter doctorAdapter;
 
     public Person login (PersonLoginDto data) throws Exception {
         Person person = findByCpf(data.getCpf());
-        Boolean isLogin = HashPassword.verifyPassword(data.getPassword(), data.getPassword());
+        Boolean isAuth = HashPassword.verifyPassword(data.getPassword(), data.getPassword());
 
-        if (isLogin) {
-            return person;
+        if (!isAuth) {
+            throw new Exception("Username or password invalid");
         }
 
-        throw new Exception("Username or password invalid");
+        Admin admin = adminAdapter.fromDto(adminService.getById(person.getId()));
+
+        if (Objects.nonNull(admin)) {
+            return admin;
+        }
+
+        return doctorAdapter.fromDto(doctorService.getById(person.getId()));
+
     }
 
     public Person findByCpf(String cpf) {
