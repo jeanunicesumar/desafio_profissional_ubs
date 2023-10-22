f3c03e8747625b625a716fa50ab9644b0d469885
Cassia <ra-22265008-2@alunos.unicesumar.edu.br>
2023-10-22 09:15:20 -0300

    cors

--- back-api/api-new/src/main/java/com/saude/agenda/api/ApiApplication.java
+++ back-api/api-new/src/main/java/com/saude/agenda/api/ApiApplication.java
@@ -2,8 +2,10 @@ package com.saude.agenda.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class ApiApplication {
 
 	// TODO: Resolver problema medicalRecordController
--- painel-gestao/painel-gestao/src/app/auth.service.ts
+++ painel-gestao/painel-gestao/src/app/auth.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
-  autenticacaoMedico(): boolean {
+  private baseUrl = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) { }
+
+  // autenticacaoMedico(cpf: string, password: string): Observable<boolean> {
+  //   return this.http.post<boolean>(`${this.baseUrl}/medico`, { cpf, password });
+  // }
+  autenticacaoMedico(){
     return false;
   }
   autenticacaoAdmnistrador(): boolean {
