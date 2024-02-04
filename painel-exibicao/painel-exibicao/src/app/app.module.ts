63ba340817ab18a3dec9a0872683bc9ca90a69cc
jeanunicesumar <jeansoares@alunos.unicesumar.edu.br>
2024-02-04 14:13:51 -0300

    sei la

--- painel-exibicao/painel-exibicao/src/app/app.module.ts
+++ painel-exibicao/painel-exibicao/src/app/app.module.ts
@@ -4,24 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TesteComponent } from './teste/teste.component';
-<<<<<<< HEAD
 import { Teste2Component } from './teste2/teste2.component';
 import { Teste3Component } from './teste3/teste3.component';
 import { Teste4Component } from './teste4/teste4.component';
-=======
->>>>>>> fa3b61d2f1230accb0829c7b01245bc665f00e17
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     TesteComponent,
     Teste2Component,
     Teste3Component,
     Teste4Component,
-=======
-    TesteComponent
->>>>>>> fa3b61d2f1230accb0829c7b01245bc665f00e17
   ],
   imports: [
     BrowserModule,
--- painel-exibicao/painel-exibicao/src/app/teste/teste.component.ts
+++ painel-exibicao/painel-exibicao/src/app/teste/teste.component.ts
@@ -7,9 +7,4 @@ import { Component } from '@angular/core';
 })
 export class TesteComponent {
 
-<<<<<<< HEAD
-=======
-  public teste2: String = 'Mundo';
-
->>>>>>> fa3b61d2f1230accb0829c7b01245bc665f00e17
 }
